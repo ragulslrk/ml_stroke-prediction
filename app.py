2d1e0a76af2a02141ce96b078335638029ecc69a
ragulslrk <slrk4444@gmail.com>
2022-06-24 21:58:10 +0530

    this is for id3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import datetime
 from flask_pymongo import PyMongo
 import pickle
-
+from chefboost import Chefboost as ch
 #flask and pymongo intialization
 app=Flask(__name__,template_folder='template',static_url_path='/static')
 app.config['SECRET_KEY']='form project'
@@ -16,6 +16,8 @@ db = mongodb_client.db
 #ml  model loading
 f= open('stroke_model','rb') 
 mp=pickle.load(f)
+
+model_id3 = ch.load_model("tennis_model.pkl")   
 
 
 @app.route('/')
@@ -56,7 +58,42 @@ def result(name,res):
          msg='Stroke predicted Positive'
 
     return render_template('result.html',names=name,msgs=msg)
+
+
+#route for id3 model
+@app.route('/id3_model')
+def id3():
+   return  render_template('id3_form.html')
+
+@app.route('/predict_id3',methods=['POST'])
+def predict_id3():
+    if request.method=="POST":
+         
+         predict_id3=ch.predict(model_id3,param=[request.form['outlook'],request.form['temp'],request.form['humidity'],request.form['wind'] ])
+         print(request.form)
+         print(predict_id3)
+         if predict_id3=="No":
+            pre=0
+         else:
+            pre=1
+         
+         return  redirect(url_for('result_id3',res=pre))
+
+@app.route("/result_id3/<int:res>")
+def result_id3(res):
+    print(res)
+    if res==0:
+        msg="You Can't Play"
+    else:
+         msg='You Can Play'
+
+    return render_template('result_id3.html',msgs=msg)
         
+#@app.route('/download')
+#def downloadFile ():
+    
+    #path = "./healthcare-dataset-stroke-data.csv"
+    #return send_file(path, as_attachment=True)
 if __name__ =="__main__":  
     
     app.run()
